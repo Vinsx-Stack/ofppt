e275277447f8caf3572ffd8fc93c75f48266a313
Vinsx-Stack <anasspofyie@gmail.com>
2025-12-04 12:26:33 +0100

    update

--- algo/POO/Gestion du ficher/class stagaire.py
+++ algo/POO/Gestion du ficher/class stagaire.py
@@ -1,74 +1,44 @@
-import csv
-
 class Stagiaire:
-    c = 1
-    def __init__(self, nom, prenom, note, filiere):
-        self.__code = Stagiaire.c
-        self.__nom = nom
-        self.__prenom = prenom
-        self.__note = note
-        self.__filiere = filiere
-        Stagiaire.c += 1
-
+    c=1
+    def __init__(self,name,aka,filiere):
+        self.Numero=Stagiaire.c
+        self.__nom=name
+        self.__prenom=aka
+        self.__filiere=filiere
+        Stagiaire.c+=1
     @property
-    def code(self):
-        return self.__code
-
-    @property
-    def nom(self):
+    def Nom(self):
         return self.__nom
-    @nom.setter
-    def nom(self, new):
-        self.__nom = new
-
+    @Nom.setter
+    def Nom(self,n):
+        self.__nom=n
     @property
-    def prenom(self):
+    def Prenom(self):
         return self.__prenom
-    @prenom.setter
-    def prenom(self, new):
-        self.__prenom = new
-
+    @Prenom.setter
+    def Prenom(self,n):
+        self.__prenom=n
     @property
-    def note(self):
-        return self.__note
-    @note.setter
-    def note(self, new):
-        self.__note = new
-
-    @property
-    def filiere(self):
+    def Filiere(self):
         return self.__filiere
-    @filiere.setter
-    def filiere(self, new):
-        self.__filiere = new
-
+    @Filiere.setter
+    def Filiere(self,n):
+        self.__filiere=n
     def __str__(self):
-        return f"{self.__code}\t{self.__nom}\t{self.__prenom}\t{self.__note}\t{self.__filiere}"
+        return f"{self.Numero}\t{self.__nom}\t{self.__prenom}\t{self.__filiere}"
 
-
-if __name__ == "__main__":
-    Lstg = []
-    s1 = Stagiaire("Anas", "Zaami", 16, "DEV")
-    s2 = Stagiaire("Rym", "Jane", 15, "RESEAU")
-    s3 = Stagiaire("Brown", "Charlie", 15, "DEV")
-    s4 = Stagiaire("Johnson", "Emily", 19, "RESEAU")
+if __name__=="__main__":
+    Lstg=[]
+    s1=Stagiaire("Adam","Elbahja","Dev")
+    s2=Stagiaire("Mohamed","Elmzabite","Dev")
+    s3=Stagiaire("Rayan","elsfaar","Dev")
+    s4=Stagiaire("Abdellah","yassin","Dev")
     Lstg.append(s1)
     Lstg.append(s2)
     Lstg.append(s3)
     Lstg.append(s4)
 
-
-    with open("stagiaires.csv", "w", newline="", encoding="utf-8-sig") as f:
-        writer = csv.writer(f, delimiter=";")
-        
-        writer.writerow(["Code", "Nom", "Prenom", "Note", "Filiere"])
-        
-
-        for stagiaire in Lstg:
-            writer.writerow([
-                stagiaire.code,
-                stagiaire.nom,
-                stagiaire.prenom,
-                stagiaire.note,
-                stagiaire.filiere
-            ])
+    with open("data2.csv","w") as g:
+        for s in Lstg:
+            g.write(str(s.Numero)+";"+ s.Nom +";" + s.Prenom +";" + s.Filiere + "\n")
+    print("Exportation avec succes ")
