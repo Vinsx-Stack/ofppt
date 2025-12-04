3e40db8c25c351fc59935202680dab57c8c7476f
Vinsx-Stack <anasspofyie@gmail.com>
2025-12-04 11:38:10 +0100

    update

---
+++ algo/POO/Gestion du ficher/omp.py
@@ -0,0 +1,4 @@
+chemain="C:\\Users\\Anas Zaami\\Downloads\\Book(Sheet1).csv"
+with open(chemain,"r") as F:
+    A=F.read()
+print(A)
---
+++ algo/POO/Gestion du ficher/write m2.py
@@ -0,0 +1,4 @@
+chemain="data.txt"
+with open(chemain,"r") as F:
+    A=F.read()
+print(A)
